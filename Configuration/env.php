e89f33247493a4492dc8fd9897082762ad7ac593
Rashed alam <rashedalamj@gmail.com>
2023-01-04 01:35:06 +0600

    environment variable added

---
+++ Configuration/env.php
@@ -0,0 +1,14 @@
+<?php
+
+//application information
+$APP_URL = "";
+$APP_FOLDER = "";
+//-----------------------
+
+//mysql database information
+$MSDB_HOST = "";
+$MSDB_PORT = "";
+$MSDB_USERNAME = "";
+$MSDB_PASSWORD = "";
+$MSDB_NAME = "";
+//--------------------------
