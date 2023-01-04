54afef67e23f3b3cb2be432d9e491817c469de3e
Rashed alam <rashedalamj@gmail.com>
2023-01-04 15:46:28 +0600

    some required function added

---
+++ Management/Functions/Functions.php
@@ -0,0 +1,25 @@
+<?php
+
+function view(string $view_file, ...$data): void
+{
+    if (file_exists(APP_ROOT . "/Resources/View/" . $view_file . ".php")) {
+        include_once(APP_ROOT . "/Resources/View/" . $view_file . ".php");
+    } else {
+        die("This view file ($view_file) are not exist !");
+    }
+}
+
+function storage_path(string $path = ''): string
+{
+    return APP_ROOT . "/Public/Storage/" . $path;
+}
+
+function assets(string $asset = ''): string
+{
+    return APP_URL . "Public/Assets/" . $asset;
+}
+
+function url(string $path = ''): string
+{
+    return APP_URL . $path;
+}
