db4936403b6e9e55267ad248b1c676f356b6ec1d
Rashed alam <rashedalamj@gmail.com>
2023-01-10 01:25:28 +0600

    Router Class added

---
+++ Management/Classes/Router.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Management\Classes;
+
+class Router
+{
+    private static function get_uri(): string
+    {
+        return $_SERVER["REQUEST_URI"];
+    }
+    private static function url_matches(string $url): array|bool
+    {
+        (string) $subject = self::get_uri();
+        if (preg_match($url, $subject, $matches)) {
+            return $matches;
+        }
+        return false;
+    }
+    private static function parse_url(string $url): string
+    {
+        $parse_url = explode("/", $url);
+        $final_url = "";
+        foreach ($parse_url as $value) {
+            if ($value) {
+                $last = strlen($value) - 1;
+                if ($value[0] === "{" && $value[$last] === "}") {
+                    $final_url .= "/(\w+)";
+                } else {
+                    $final_url .= "/$value";
+                }
+            }
+        }
+        return $final_url;
+    }
+    private static function process(string $url, object|array $callback): void
+    {
+        (string) $pattern = self::parse_url($url);
+        (string) $url_match = "~^{$pattern}/?$~";
+        (array)(bool) $params = self::url_matches($url_match);
+        if ($params) {
+            $arguments = array_slice($params, 1);
+            if (is_callable($callback)) {
+                $callback(...$arguments);
+            } else if (is_array($callback)) {
+                (string) $class = $callback[0];
+                (string) $method = $callback[1];
+                (object) $object = new $class();
+                $object->$method(...$arguments);
+            }
+        }
+    }
+    public static function get(string $url, object|array $callback)
+    {
+        if ($_SERVER["REQUEST_METHOD"] != "GET") {
+            return;
+        }
+        self::process($url, $callback);
+    }
+    public static function put(string $url, object|array $callback)
+    {
+        if ($_SERVER["REQUEST_METHOD"] != "PUT") {
+            return;
+        }
+        self::process($url, $callback);
+    }
+    public static function delete(string $url, object|array $callback)
+    {
+        if ($_SERVER["REQUEST_METHOD"] != "DELETE") {
+            return;
+        }
+        self::process($url, $callback);
+    }
+    public static function post(string $url, object|array $callback)
+    {
+        if ($_SERVER["REQUEST_METHOD"] != "POST") {
+            return;
+        }
+        self::process($url, $callback);
+    }
+}
