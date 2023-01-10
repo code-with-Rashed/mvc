e9547d563c1d7801d3d166235bcc16bc520dc87d
Rashed alam <rashedalamj@gmail.com>
2023-01-11 05:06:14 +0600

    view file adding facility added

---
+++ Resources/View/testView.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>test</title>
+    <link rel="stylesheet" href="<?php echo assets("style.css")?>">
+</head>
+<body>
+    <h1 class="color">Hello everyone</h1>
+    <a href="<?php echo stored_file("test.txt");?>">test</a>
+</body>
+</html>
