f96c8ac24f67a86eb251510b7ac1d4d835b2d302
Rashed alam <rashedalamj@gmail.com>
2023-03-29 23:58:46 +0600

    Data filtration feature added

---
+++ Management/Classes/Filtration.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Management\Classes;
+
+class Filtration
+{
+    public static function filter(array $data)
+    {
+        foreach ($data as $key => $value) {
+            $value = trim($value);
+            $value = stripslashes($value);
+            $value = strip_tags($value);
+            $value = htmlspecialchars($value);
+            $data[$key] = $value;
+        }
+        return $data;
+    }
+}
